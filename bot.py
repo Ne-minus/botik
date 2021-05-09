81c4e14eccc23dfd5b0a7ad1633b11348d000ce7
Server1 <trial@example.com>
2021-05-09 14:39:58 +0000

    1

--- bot.py
+++ bot.py
@@ -6,6 +6,11 @@ TOKEN = input("Введите токен:")  # '1663223369:AAH-yDDUkiJG33lUV5ZmH
 bot = telebot.TeleBot(TOKEN)
 
 
+@bot.message_handler(func=lambda message: message.forward_from.username == 'semicodebot')
+def update_hw(message):
+    bot.send_message(call.message.chat.id, "семикод?")
+
+
 @bot.message_handler(content_types=['text', '/reg'])
 def asker(message):
     if 'задач' in message.text and 'Все задачи по' not in message.text:
