264c5eb29907b4a9306ec8820bcaf41645a422c2
Server1 <trial@example.com>
2021-05-09 20:37:49 +0000

    1

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ def update_hw(message):
         with urllib.request.urlopen(rep_link) as r:
             task_links = []
             pattern_task_link = r'(?<=<a href=")/Pandaklez/[0-9a-z]+/raw/[0-9a-z]+/[0-9]+[.]{1}md(?=")'  # регуляяяяяр_очка
-            pattern_hw = r'(?<=<title>)[HWhw-]+[0-9]+'
+            pattern_hw = r'(?<=[HWhw-])+[0-9]+'
             for line in r:
                 decoded = line.decode('utf-8')
                 if '<a href="/Pandaklez' in decoded and '/raw/' in decoded:
