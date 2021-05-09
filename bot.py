47cbd31e6f78e80ca69443450c45eb7a293b097e
Server1 <trial@example.com>
2021-05-09 20:39:51 +0000

    bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ def update_hw(message):
                         task_links.append('https://gist.githubusercontent.com'+n)
                 elif '<title>' in decoded:
                     hw = re.findall(pattern_hw, decoded)[0]
-        bot.send_message(message.chat.id, task_links[1])
+                    bot.send_message(message.chat.id, hw)
 
 
 @bot.message_handler(content_types=['text', '/reg'])
